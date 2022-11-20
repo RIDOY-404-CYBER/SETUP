f128ad91a4860f4de86939c4ce8fce68b96199f6
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-11-20 12:37:17 +0600

    Create SET-UP.py

---
+++ SET-UP.py
@@ -0,0 +1,47 @@
+import os
+
+ 
+
+  
+ 
+
+ 
+
+os.system('git pull')
+
+ 
+
+ 
+
+
+
+
+
+
+
+
+
+
+os.system('xdg-open https://facebook.com/groups/770617227293870/')
+ 
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+ 
+
+import PKG
+
+
+
+
+
+
+PKG.menu()
